408d3d95a66cfab0b01fb85d97671d5597b92d3e
dea710 <dea770@naver.co>
2012-11-13 18:42:44 +0900

    main update

--- main/js/jquery.index.js
+++ main/js/jquery.index.js
@@ -1,13 +1,51 @@
 $(function($){
 	$(function(){
 	var
-	$mid = $('#other_mid');
+	$mid = $('#other_mid'),/*공지사항*/
+	$bar = $('#navibar'),/*메뉴*/
+	$thema = $('#other_right');/*테마*/
+	/*공지사항*/
+	$("#other_mid>ul>li>div").hide();
 	$mid.find('li>a>span').bind('mouseover focusin',function(){
 		$("#other_mid>ul>li").removeClass('selected');
+		$("#other_mid>ul>li>a>span").removeClass('selected');
 		$("#other_mid>ul>li>div").hide();
+		$(this).addClass('selected');
 		$(this).parent().parent().addClass('selected');
 		$(this).parent().parent().find('div').show();
 	});
+	/*공지사항 끝*/
+	
+
+	
+	/* 테마 */
+	$("#other_right>ul>li>div").hide();
+	$thema.find('li>a>span').bind('mouseover focusin',function(){
+		$("#other_right>ul>li").removeClass('selected');
+		$("#other_right>ul>li>a>span").removeClass('selected');
+		$("#other_right>ul>li>div").hide();
+		$(this).addClass('selected');
+		$(this).parent().parent().addClass('selected');
+		$(this).parent().parent().find('div').show();
+	});
+	/* 테마끝 */
+	
+	
+	
+	/* 메뉴  */
+	$bar.find('li').bind('mouseover focusin',function(){
+		$("#navibar>ul>li").removeClass('selected');
+		$("#navibar>ul>li>a>span").removeClass('selected');
+		/*$("#navibar>ul>li>div").hide();*/
+		$(this).addClass('selected');
+		$(this).parent().parent().addClass('selected');
+		$(this).parent().parent().find('div').show();
+		$(this).addClass('selected').siblings().removeClass('selected');
+	});
+	$bar.find('li').bind('mouseout focusout',function(){
+		$(this).removeClass('selected');
+	});
+	/* 메뉴 끝 */
 	$("#other_mid li>a:first").click();
 	});
 })(jQuery);
