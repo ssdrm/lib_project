c6c46077e05e20ed0f0e30f51eea76331b0046b7
Seungse <ssdrm@chol.com>
2012-11-23 19:19:52 +0900

    121123

--- WebContent/js/jquery.index.js
+++ WebContent/js/jquery.index.js
@@ -3,7 +3,9 @@ $(function($){
 	var
 	$mid = $('#other_mid'),/*공지사항*/
 	$bar = $('#navibar'),/*메뉴*/
-	$thema = $('#other_right');/*테마*/
+	$thema = $('#other_right'),/*테마*/
+	$banner = $('#other_left');/*배너*/
+
 	/*공지사항*/
 	/* 초기 화면 */
 	$("#other_mid>ul>li>div").hide();
@@ -40,7 +42,23 @@ $(function($){
 	});
 	/* 테마끝 */
 	
-	
+	/* 배너 시작*/
+
+	$("#other_left>ul>li>div").hide();
+	$banner.find('li:first>span>img').addClass('selected');
+	$banner.find('li:first').addClass('selected');
+	$banner.find('li:first>div').show();
+
+	$banner.find('li>span>img').bind('mouseover focusin',function(){
+		$("#other_left>ul>li").removeClass('selected');
+		$("#other_left>ul>li>span>img").removeClass('selected');
+		$("#other_left>ul>li>div").hide();
+		$(this).addClass('selected');
+		$(this).parent().parent().addClass('seleted');
+		$(this).parent().parent().find('div').show();
+	});
+
+
 	
 	/* 메뉴  */
 
